b65f4ea82ff5a1bc68b7897ab20ded08cc980bc8
Adeel Anwar <adeel.anwar144.aa@gmail.com>
2026-06-05 09:02:08 +0500

    fix cache files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // disable build cache
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
